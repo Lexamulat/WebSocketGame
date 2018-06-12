f1a140375ceb0423566cb75f1265a36088379d56
lexamulat <lexamulat@mail.ru>
2018-06-12 10:45:23 +0300

    final

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 use: npm i 
 //for dependences
 
-start server  at "./server/server.js"
+create bundle.js by using "npm run build" in "WebsocketGame"
+
+start server: "WebsocketGame/server"
 
 node server.js
 ///
 server starting at 
-
 http://localhost:9999/
+////
+go to http://localhost:9999/ 
+in browser
 
 You will see a waiting screen, copy this link into other browser tab.
 Game starting. 
--- src/screens/GameScreen/OpponentsPart/OpponentsCard.jsx
+++ src/screens/GameScreen/OpponentsPart/OpponentsCard.jsx
@@ -24,9 +24,8 @@ class OpponentsCard extends React.Component {
     }
 
     displayThisCard(){
-        return <div className="cardFrame"> <svg className="svgicon " xmlns="http://www.w3.org/2000/svg " viewBox="0 0 32 32 "><path d="M27 4l-15 15-7-7-5 5 12 12 20-20z"></path>
-        </svg>
-        </div>
+        return <div className="cardFrame"> <div className="greencard"></div>
+        </div> 
     }
     
     displayUnknownCard(){
